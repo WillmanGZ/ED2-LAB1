f65ff0638a5a26c2057575bd082b935b0845fc8e
Willman Giraldo Zambrano <143544629+WillmanGZ@users.noreply.github.com>
2024-09-17 19:39:36 -0500

    Se agregaron respuesta

--- LAB1/src/lab1/Main.java
+++ LAB1/src/lab1/Main.java
@@ -10,6 +10,14 @@ public class Main {
         arbol.agregarNodo("Llegada a la Luna", "Historia", "En que ano llego el hombre a la luna", "1969", 3, arbol.raiz);
         arbol.agregarNodo("Pais mas Grande", "Geografia", "Cual es el pais mas grande del mundo por superficie", "Rusia", 4, arbol.raiz);
         arbol.agregarNodo("Jugadores de Futbol", "Deportes", "Cuantos jugadores hay en un equipo de futbol en el campo", "11", 5, arbol.raiz);
+        arbol.agregarNodo("Inventor de Microsoft", "Tecnologia", "Bill Gates es el fundador de Microsoft", "Verdadero", 1, arbol.raiz);
+        arbol.agregarNodo("Tabla periodica", "Ciencias", "La tabla periodica tiene 118 elementos", "Verdadero", 2, arbol.raiz);
+        arbol.agregarNodo("El Renacimiento", "Arte", "El Renacimiento fue un movimiento artistico del siglo XV", "Verdadero", 3, arbol.raiz);
+        arbol.agregarNodo("Torre Eiffel", "Cultura Pop", "La Torre Eiffel esta en Berlin", "Falso", 5, arbol.raiz);
+        arbol.agregarNodo("El baloncesto", "Deportes", "El baloncesto fue inventado en Estados Unidos", "Verdadero", 4, arbol.raiz);
+        arbol.agregarNodo("La independencia", "Historia", "La independencia de Mexico se declaro en 1821", "Falso", 8, arbol.raiz);
+        arbol.agregarNodo("Rayos", "Ciencias", "Los rayos siempre caen en el mismo lugar", "Falso", 3, arbol.raiz);
+
         arbol.iniciarJuego();
     }
 
